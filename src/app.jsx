de4a1f7dbf002242cac421630dbaa1d8e36af2b4
Abdullah Hussein <abdullabale11@gmail.com>
2024-10-14 13:45:41 +0300

    fix

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,8 @@
 import "./app.css";
 export default function App() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center"></div>
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      placeholder
+    </div>
   );
 }
